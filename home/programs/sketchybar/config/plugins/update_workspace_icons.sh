1a710b72274467bd2e485af5e592fcf7c42ee4b4
Mate Vasarhelyi <mate.vasarhelyi@alpheya.com>
2025-07-03 10:33:25 +0200

    feat: sketchybar empty workspace draw on focus

--- home/programs/sketchybar/config/plugins/update_workspace_icons.sh
+++ home/programs/sketchybar/config/plugins/update_workspace_icons.sh
@@ -3,16 +3,25 @@
 CONFIG_DIR="$HOME/.config/sketchybar"
 
 sid=$1
+focused_sid=$2
 apps=$(/etc/profiles/per-user/mate/bin/aerospace list-windows --workspace "$sid" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
 
-if [ "${apps}" != "" ]; then
+if [ "$sid" = "$focused_sid" ]; then
     sketchybar --set ws.$sid drawing=on
+fi
+
+if [ "${apps}" != "" ]; then
+    if [ "$sid" != "$focused_sid" ]; then
+        sketchybar --set ws.$sid drawing=on
+    fi
     icon_strip=" "
     while read -r app; do
         icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
     done <<<"${apps}"
 else
     icon_strip=""
-    sketchybar --set ws.$sid drawing=off
+    if [ "$sid" != "$focused_sid" ]; then
+        sketchybar --set ws.$sid drawing=off
+    fi
 fi
 sketchybar --set ws.$sid label="$icon_strip"
--- home/programs/sketchybar/config/plugins/workspace.sh
+++ home/programs/sketchybar/config/plugins/workspace.sh
@@ -4,8 +4,7 @@ CONFIG_DIR="$HOME/.config/sketchybar"
 
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME drawing=on
-  $CONFIG_DIR/plugins/update_workspace_icons.sh "$1"
+  $CONFIG_DIR/plugins/update_workspace_icons.sh "$1" "$FOCUSED_WORKSPACE"
   sketchybar --set $NAME background.color=0x88FF00FF label.shadow.drawing=on icon.shadow.drawing=on background.border_width=2
 else
   sketchybar --set $NAME background.color=0x44FFFFFF label.shadow.drawing=off icon.shadow.drawing=off background.border_width=0
